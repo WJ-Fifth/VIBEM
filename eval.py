bb646dce77dd206fe37b0ee37d4ab73b7ad2495c
jinwu <wjw104655073@outlook.com>
2022-10-31 13:43:35 +1100

    Improvement experiments base on VIBE

---
+++ eval.py
@@ -0,0 +1,64 @@
+import os
+import torch
+
+from lib.dataset import ThreeDPW
+from lib.dataset import ssp3d
+from lib.models import VIBE
+from lib.core.evaluate import Evaluator
+from lib.core.config import parse_args
+from torch.utils.data import DataLoader
+
+
+def main(cfg):
+    print('...Evaluating on 3DPW test set...')
+
+    model = VIBE(
+        n_layers=cfg.MODEL.TGRU.NUM_LAYERS,
+        batch_size=cfg.TRAIN.BATCH_SIZE,
+        seqlen=cfg.DATASET.SEQLEN,
+        hidden_size=cfg.MODEL.TGRU.HIDDEN_SIZE,
+        pretrained=cfg.TRAIN.PRETRAINED_REGRESSOR,
+        add_linear=cfg.MODEL.TGRU.ADD_LINEAR,
+        bidirectional=cfg.MODEL.TGRU.BIDIRECTIONAL,
+        use_residual=cfg.MODEL.TGRU.RESIDUAL,
+    ).to(cfg.DEVICE)
+
+    if cfg.TRAIN.PRETRAINED != '' and os.path.isfile(cfg.TRAIN.PRETRAINED):
+        checkpoint = torch.load(cfg.TRAIN.PRETRAINED)
+        best_performance = checkpoint['performance']
+        model.load_state_dict(checkpoint['gen_state_dict'])
+        print(f'==> Loaded pretrained model from {cfg.TRAIN.PRETRAINED}...')
+        if cfg.TRAIN.DATASET_EVAL == 'ThreeDPW':
+            print(f'Performance on 3DPW test set {best_performance}')
+        elif cfg.TRAIN.DATASET_EVAL == 'ssp3d':
+            print(f'Performance on ssp_3D test set {best_performance}')
+    else:
+        print(f'{cfg.TRAIN.PRETRAINED} is not a pretrained model!!!!')
+        exit()
+
+    if cfg.TRAIN.DATASET_EVAL == 'ThreeDPW':
+        test_db = ThreeDPW(set='test', seqlen=cfg.DATASET.SEQLEN, debug=cfg.DEBUG)
+    elif cfg.TRAIN.DATASET_EVAL == 'ssp3d':
+        test_db = ssp3d(set='test', seqlen=cfg.DATASET.SEQLEN, debug=cfg.DEBUG)
+    else:
+        print(f'{cfg.DATASET_EVAL} is not a available dataset!!!!')
+        exit()
+
+    test_loader = DataLoader(
+        dataset=test_db,
+        batch_size=cfg.TRAIN.BATCH_SIZE,
+        shuffle=False,
+        num_workers=cfg.NUM_WORKERS,
+    )
+
+    Evaluator(
+        model=model,
+        device=cfg.DEVICE,
+        test_loader=test_loader,
+    ).run()
+
+
+if __name__ == '__main__':
+    cfg, cfg_file = parse_args()
+
+    main(cfg)
---
+++ lib/data_utils/ssp3d_utils.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+
+# Max-Planck-Gesellschaft zur Förderung der Wissenschaften e.V. (MPG) is
+# holder of all proprietary rights on this computer program.
+# You can only use this computer program if you have closed
+# a license agreement with MPG or you get the right to use the computer
+# program from someone who is authorized to grant you that right.
+# Any use of the computer program without a valid license is prohibited and
+# liable to prosecution.
+#
+# Copyright©2019 Max-Planck-Gesellschaft zur Förderung
+# der Wissenschaften e.V. (MPG). acting on behalf of its Max Planck Institute
+# for Intelligent Systems. All rights reserved.
+#
+# Contact: ps-license@tuebingen.mpg.de
+
+import sys
+
+sys.path.append('.')
+
+import os
+import cv2
+import torch
+import joblib
+import argparse
+import numpy as np
+import pickle as pkl
+import os.path as osp
+from tqdm import tqdm
+
+from lib.models import spin
+from lib.data_utils.kp_utils import *
+from lib.core.config import VIBE_DB_DIR, VIBE_DATA_DIR
+from lib.utils.smooth_bbox import get_smooth_bbox_params
+from lib.models.smpl import SMPL, SMPL_MODEL_DIR, H36M_TO_J14
+from lib.data_utils.feature_extractor import extract_features
+from lib.utils.geometry import batch_rodrigues, rotation_matrix_to_angle_axis
+
+NUM_JOINTS = 24
+VIS_THRESH = 0.3
+MIN_KP = 6
+
+
+def read_data(folder):
+    dataset = {
+        'vid_name': [],
+        'frame_id': [],
+        'joints3D': [],
+        'joints2D': [],
+        'shape': [],
+        'pose': [],
+        'bbox': [],
+        'img_name': [],
+        'features': [],
+        # 'valid': [],
+    }
+
+    model = spin.get_pretrained_hmr()
+
+    smpl = SMPL(SMPL_MODEL_DIR, batch_size=1, create_transl=False)
+
+    data_file = osp.join(folder, 'labels.npz')
+
+    data = np.load(data_file)
+
+    img_dir = osp.join(folder, 'images')
+    silhouettes_dir = osp.join(folder, 'silhouettes')
+
+    num_people = len(data['poses'])
+    num_frames = len(data['fnames'])
+    # print(data['fnames'][0])
+
+    assert (data['joints2D'].shape[0] == num_frames)
+
+    pose = torch.from_numpy(data['poses']).float()
+    shape = torch.from_numpy(data['shapes']).float()
+    trans = torch.from_numpy(data['cam_trans']).float()
+
+    gender = data['genders']
+
+    bbox_centre = data['bbox_centres']
+    bbox_whs = data['bbox_whs']
+
+    j2d = data['joints2D']
+
+    output = smpl(betas=shape, body_pose=pose[:, 3:], global_orient=pose[:, :3], transl=trans)
+    j3d = output.joints
+
+    bbox = np.vstack([bbox_centre[:, 0], bbox_centre[:, 1], bbox_whs, bbox_whs]).T
+
+    bbox_params, time_pt1, time_pt2 = get_smooth_bbox_params(j2d, vis_thresh=VIS_THRESH, sigma=8)
+
+    img_paths = []
+    vid_names = []
+    for i_fname in data['fnames']:
+        img_path = osp.join(img_dir, i_fname)
+        img_paths.append(img_path)
+        name_split = i_fname.split('_')
+        if name_split[3] == 'clip':
+            vid_names.append(name_split[0] + '_vid_' + name_split[2] + '_clip_' + name_split[4] + '_person_' + name_split[6])
+        else:
+            vid_names.append(name_split[0]+'_'+name_split[1]+'_vid_' +
+                             name_split[3] + '_clip_' + name_split[5] + '_person_' + name_split[7])
+
+    img_paths_array = np.array(img_paths)[time_pt1:time_pt2]
+    dataset['vid_name'] = np.array(vid_names)[time_pt1:time_pt2]
+    # print(dataset['vid_name'].shape)
+    dataset['frame_id'] = data['fnames'][time_pt1:time_pt2]
+    dataset['img_name'] = img_paths_array
+    dataset['joints3D'] = j3d.numpy()
+    dataset['joints2D'] = j2d
+    dataset['shape'] = shape.numpy()
+    dataset['pose'] = pose.numpy()
+    dataset['bbox'] = bbox
+    # dataset['valid'].append(campose_valid[time_pt1:time_pt2])
+
+    features = extract_features(model, img_paths_array, bbox,
+                                kp_2d=j2d[time_pt1:time_pt2], debug=debug, dataset='3dpw', scale=1.2)
+
+    dataset['features'] = features
+
+    return dataset
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', type=str, help='dataset directory', default='data/ssp_3d')
+    args = parser.parse_args()
+
+    debug = False
+
+    dataset = read_data(args.dir)
+    # print(dataset['vid_name'])
+    # exit()
+    joblib.dump(dataset, osp.join(VIBE_DB_DIR, 'ssp3d_test_db.pt'))
+    print("ssp3d_test_db set success !!!")
